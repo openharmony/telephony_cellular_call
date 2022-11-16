3f438771f8355d5e8ab2c75fddea8a3643a40499
YOUR_NAME <liujiandong1@huawei.com>
2022-11-16 16:49:33 +0800

    fixed a7fba3e from https://gitee.com/life-liu/telephony_cellular_call/pulls/277 fix: add fuzz Signed-off-by: liujiandong <liujiandong1@huawei.com>

---
+++ test/fuzztest/setimsfeaturevalue_fuzzer/setimsfeaturevalue_fuzzer.cpp
@@ -0,0 +1,208 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "setimsfeaturevalue_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+#define private public
+#include "addcellularcalltoken_fuzzer.h"
+#include "cellular_call_service.h"
+#include "system_ability_definition.h"
+
+using namespace OHOS::Telephony;
+namespace OHOS {
+static bool g_isInited = false;
+constexpr int32_t BOOL_NUM = 2;
+constexpr int32_t INT_NUM = 2;
+
+bool IsServiceInited()
+{
+    DelayedSingleton<CellularCallService>::GetInstance()->OnStart();
+    if (!g_isInited && (static_cast<int32_t>(DelayedSingleton<CellularCallService>::GetInstance()->state_) ==
+                           static_cast<int32_t>(ServiceRunningState::STATE_RUNNING))) {
+        g_isInited = true;
+    }
+    return g_isInited;
+}
+
+void OnRemoteRequest(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    MessageParcel dataMessageParcel;
+    if (!dataMessageParcel.WriteInterfaceToken(CellularCallStub::GetDescriptor())) {
+        return;
+    }
+    size_t dataSize = size - sizeof(uint32_t);
+    dataMessageParcel.WriteBuffer(data + sizeof(uint32_t), dataSize);
+    dataMessageParcel.RewindRead(0);
+    uint32_t code = static_cast<uint32_t>(size);
+    MessageParcel reply;
+    MessageOption option;
+    DelayedSingleton<CellularCallService>::GetInstance()->OnRemoteRequest(code, dataMessageParcel, reply, option);
+}
+
+void SetDomainPreferenceMode(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    int32_t mode = static_cast<int32_t>(size);
+    MessageParcel dataMessageParcel;
+    dataMessageParcel.WriteInt32(mode);
+    size_t dataSize = size - sizeof(int32_t);
+    dataMessageParcel.WriteBuffer(data + sizeof(int32_t), dataSize);
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CellularCallService>::GetInstance()->OnSetDomainPreferenceModeInner(dataMessageParcel, reply);
+}
+
+void GetDomainPreferenceMode(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    MessageParcel dataMessageParcel;
+    dataMessageParcel.WriteBuffer(data, size);
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CellularCallService>::GetInstance()->OnSetDomainPreferenceModeInner(dataMessageParcel, reply);
+}
+
+void SetImsSwitchStatus(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    int32_t activate = static_cast<int32_t>(size % BOOL_NUM);
+    MessageParcel dataMessageParcel;
+    dataMessageParcel.WriteBool(activate);
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CellularCallService>::GetInstance()->OnSetImsSwitchStatusInner(dataMessageParcel, reply);
+}
+
+void GetImsSwitchStatus(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    MessageParcel dataMessageParcel;
+    dataMessageParcel.WriteBuffer(data, size);
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CellularCallService>::GetInstance()->OnGetImsSwitchStatusInner(dataMessageParcel, reply);
+}
+
+void GetImsConfig(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    int32_t item = static_cast<int32_t>(size);
+    MessageParcel dataMessageParcel;
+    dataMessageParcel.WriteInt32(item);
+    size_t dataSize = size - sizeof(int32_t);
+    dataMessageParcel.WriteBuffer(data + sizeof(int32_t), dataSize);
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CellularCallService>::GetInstance()->OnGetImsConfigInner(dataMessageParcel, reply);
+}
+
+void SetImsConfig(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    int32_t item = static_cast<int32_t>(size);
+    std::string value(reinterpret_cast<const char *>(data), size);
+    MessageParcel dataMessageParcel;
+    dataMessageParcel.WriteInt32(item);
+    dataMessageParcel.WriteString(value);
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CellularCallService>::GetInstance()->OnSetImsConfigStringInner(dataMessageParcel, reply);
+}
+
+void GetImsFeatureValue(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    int32_t type = static_cast<int32_t>(size);
+    MessageParcel dataMessageParcel;
+    dataMessageParcel.WriteInt32(type);
+    size_t dataSize = size - sizeof(int32_t);
+    dataMessageParcel.WriteBuffer(data + sizeof(int32_t), dataSize);
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CellularCallService>::GetInstance()->OnGetImsFeatureValueInner(dataMessageParcel, reply);
+}
+
+void SetImsFeatureValue(const uint8_t *data, size_t size)
+{
+    if (!IsServiceInited()) {
+        return;
+    }
+
+    int32_t type = static_cast<int32_t>(size);
+    int32_t value = static_cast<int32_t>(size);
+    MessageParcel dataMessageParcel;
+    dataMessageParcel.WriteInt32(type);
+    dataMessageParcel.WriteInt32(value);
+    size_t dataSize = size - sizeof(int32_t) * INT_NUM;
+    dataMessageParcel.WriteBuffer(data + sizeof(int32_t) * INT_NUM, dataSize);
+    dataMessageParcel.RewindRead(0);
+    MessageParcel reply;
+    DelayedSingleton<CellularCallService>::GetInstance()->OnSetImsFeatureValueInner(dataMessageParcel, reply);
+}
+
+void DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
+{
+    if (data == nullptr || size == 0) {
+        return;
+    }
+
+    OnRemoteRequest(data, size);
+    SetDomainPreferenceMode(data, size);
+    GetDomainPreferenceMode(data, size);
+    SetImsSwitchStatus(data, size);
+    GetImsSwitchStatus(data, size);
+    GetImsConfig(data, size);
+    SetImsConfig(data, size);
+    GetImsFeatureValue(data, size);
+    SetImsFeatureValue(data, size);
+    return;
+}
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    OHOS::AddCellularCallTokenFuzzer token;
+    /* Run your code on data */
+    OHOS::DoSomethingInterestingWithMyAPI(data, size);
+    return 0;
+}
