28e3922227e51d1e2c170b3c4813c6c02d4d3475
闪光 <drink_thinking@qq.com>
2024-07-19 11:51:44 +0000

    update services/utils/src/cellular_call_config.cpp.
    Signed-off-by: 闪光 <drink_thinking@qq.com>

--- services/utils/src/cellular_call_config.cpp
+++ services/utils/src/cellular_call_config.cpp
@@ -367,7 +367,6 @@ void CellularCallConfig::HandleOperatorConfigChanged(int32_t slotId)
         TELEPHONY_LOGE("failed due to parse operator config");
         return;
     }
-
     ResetImsSwitch(slotId);
     UpdateImsCapabilities(slotId, true);
 }
