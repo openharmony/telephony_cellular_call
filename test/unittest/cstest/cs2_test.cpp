e19730cdcc9de49f83cc9187331026b1a21fbd3e
kunkun <zhangkunkun6@huawei.com>
2025-01-02 16:57:16 +0800

    cellular_call_CellularCallHandler_0005 dt_bugfix
    Signed-off-by: kunkun <zhangkunkun6@huawei.com>

--- test/unittest/cstest/cs2_test.cpp
+++ test/unittest/cstest/cs2_test.cpp
@@ -1310,6 +1310,7 @@ HWTEST_F(Cs2Test, cellular_call_CellularCallHandler_0005, Function | MediumTest 
     callInfoList->calls.push_back(callInfo);
     fifthHandler.ProcessRedundantCode(*callInfoList);
     EXPECT_EQ(callInfoList->calls[0].number, expectedPhoneNumber);
+    callInfoList->calls[0].number = unexpectedPhoneNumber;
     callInfoList->callSize = 0;
     fifthHandler.ProcessRedundantCode(*callInfoList);
     EXPECT_EQ(callInfoList->calls[0].number, expectedPhoneNumber);
