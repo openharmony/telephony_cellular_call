8bfb1bb135ddefcde12450149b2c810c708d0a1a
xhttt123 <2317512661@qq.com>
2025-06-03 02:12:03 +0000

    update test/mock/mock_tel_ril_manager.h.
    Signed-off-by: xhttt123 <2317512661@qq.com>

--- test/mock/mock_tel_ril_manager.h
+++ test/mock/mock_tel_ril_manager.h
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// t
+
 #ifndef MOCK_TEL_RIL_MANAGER_H
 #define MOCK_TEL_RIL_MANAGER_H
 
