6b98677ba30ec0da8470e61a387691267fb57feb
haige1234 <lijianghai3@h-partners.com>
2026-04-17 11:50:48 +0800

    fix CS 8686
    Signed-off-by: haige1234 <lijianghai3@h-partners.com>

--- services/manager/src/cellular_call_handler.cpp
+++ services/manager/src/cellular_call_handler.cpp
@@ -1525,8 +1525,8 @@ void CellularCallHandler::ProcessRedundantCode(CallInfoList &callInfoList)
             break;
         }
         std::regex phoneContextPattern(DUPLICATIVE_CN_INTERNATIONAL_NUMBER_PREFIX_FORMAT);
-        if ((callInfo.type == INTERNATION_CODE && 
-            callInfo.number.substr(0, duplicativePhoneNumberPrefix0.length()) == duplicativePhoneNumberPrefix0)) {
+        if (callInfo.type == INTERNATION_CODE && 
+            callInfo.number.substr(0, duplicativePhoneNumberPrefix0.length()) == duplicativePhoneNumberPrefix0) {
             callInfoList.calls[i].number = CN_INTERNATIONAL_NUMBER_PREFIX + 
                 callInfo.number.substr(duplicativePhoneNumberPrefix0.length());
         } else if (callInfo.type != INTERNATION_CODE && 
